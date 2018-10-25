5a113b5954db9b12215c5372e61c8815f2c0f4cb
Wilbert Del Rosario <wilbert.delrosario.2022@student.lhschools.org>
2018-10-25 08:48:21 -0400

    made stuff

--- src/Rock_Paper_Scissor.java
+++ src/Rock_Paper_Scissor.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Rock_Paper_Scissor {
 
 
+    public static void main(String[] args) {
+        Scanner keyboard;
+        int compscore = 0;
+        int userscore = 0;
+        int counter = 0;
+
+        keyboard = new Scanner(System.in);
+
+        System.out.println("Welcome to Rock,Paper,Scissors");
+
+        while (counter == 0) {
+            
 
 
 
@@ -8,20 +23,7 @@ public class Rock_Paper_Scissor {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
+    }
 }
+
